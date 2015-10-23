7ec5058c5807705276b4a430d2f92e6c08014076
astandrik <astandrik@gmail.com>
2015-10-23 10:14:36 +0300

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,56 @@
+var demoApp = angular.module('testApp',['ngRoute','ngMaterial','ngAnimate']);
+var controllers = {};
+
+$.ajaxSetup({
+    async: false
+});
+controllers.testController = function($scope) {
+    $scope.person = {name: 'Иван', surname: 'Иваныч'};
+}
+
+controllers.startPageController = function($scope) {
+
+};
+
+controllers.addNewController = function($scope) {
+}
+
+controllers.projectsController = function($scope) {
+    $scope.headers = [];
+    $scope.rows = [];
+    $.getJSON('/testData/projects.json', function(json) {
+        for(var i = 0; i < Object.keys(json[0]).length; i++) {
+           $scope.headers.push(Object.keys(json[0])[i]);
+        }
+        for(var i = 0; i < json.length; i++) {
+            var obj = [];
+            for(var j = 0; j < Object.keys(json[i]).length; j++) {
+                obj.push({key:Object.keys(json[i])[j], value: json[i][Object.keys(json[i])[j]]});
+            }
+            $scope.rows.push(obj);
+        }
+    })
+      
+ }
+
+demoApp.config(function($routeProvider) {
+    $routeProvider
+        .when('/', 
+              {
+                templateUrl: 'pages/login.html'
+              })
+        .when('/Main', 
+              { 
+                templateUrl: 'pages/startpage.html'
+              })
+        .when('/Projects', 
+              { 
+                templateUrl: 'pages/projects.html'
+              })
+        .when('/AddProject', 
+              { 
+                templateUrl: 'pages/newproject.html'
+              })    ;
+});
+
+demoApp.controller(controllers);
