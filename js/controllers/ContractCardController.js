a42fb8ce96f2a0236258377bb8c92f85da1b6204
astandrik <astandrik@gmail.com>
2015-10-30 13:20:52 +0300

    Additions

--- js/controllers/ContractCardController.js
+++ js/controllers/ContractCardController.js
@@ -12,7 +12,7 @@
         Object.keys(proj).forEach(function (key) {
             $scope[key] = proj[key];
         });
-        var headDogovor = $scope.contract_date == "" ? '( ' + $scope.contract_number + ' )' : "( " + $scope.contract_number + ' от ' + $scope.contract_date + " )";
+        var headDogovor = $scope.contract_date == "" ? '(' + $scope.contract_number + ')' : " (" + $scope.contract_number + ' от ' + $scope.contract_date + ")";
         switch (projectType) {
             case 'orderers':
                 $scope.setToolBarHeader($scope.contract_code + headDogovor);
